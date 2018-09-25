6175afa14c03223c4202e6d1f58c50cbbbc5c936
Adam Morsi <adam.morsi@yahoo.com>
2018-09-25 17:01:20 +0300

    Section 13 until 164

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs/Rx';
+import {Observer} from 'rxjs/Observer';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        // the following code is an example of creating interval observable
+        /*const myNumbers = Observable.interval(1000);
+        myNumbers.subscribe(
+            (number: number) => {
+              console.log(number);
+            }
+        );*/
+
+        const myObservable = Observable.create((observer: Observer<string>) => {
+            setTimeout(() => {
+                observer.next('first package');
+            }, 2000);
+            setTimeout(() => {
+                observer.next('second package');
+            }, 4000);
+            setTimeout(() => {
+                // observer.error('this does not work');
+                observer.complete();
+            }, 5000);
+            setTimeout(() => {
+                observer.next('third package');
+            }, 6000);
+        });
+        myObservable.subscribe(
+            (data: string) => {
+                console.log(data);
+            },
+            (error: string) => {
+                console.log(error);
+            },
+            () => {
+                console.log('completed');
+            },
+        );
+    }
 
 }
